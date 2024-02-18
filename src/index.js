7b7b02859258eff43ab4c301ecdafd7bacdcdaff
Kevin <kevin8003129@gmail.com>
2024-02-18 23:23:34 +0800

    deploy test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
-import GlobalStyles from "./components/styles/GlobalStyles";
-import { theme } from "./Global";
-import { ThemeProvider } from "styled-components";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
+import GlobalStyles from './components/styles/GlobalStyles';
+import { theme } from './Global';
+import { ThemeProvider } from 'styled-components';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    <RouterProvider router={router}></RouterProvider>
-  </ThemeProvider>
+	<ThemeProvider theme={theme}>
+		<GlobalStyles />
+		<RouterProvider router={router}></RouterProvider>
+	</ThemeProvider>
 );
+reportWebVitals();
