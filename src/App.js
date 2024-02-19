f23f5a4864b44c0ae23cde2bca664b0227c5902d
Kevin Chen <kevin8003129@gmail.com>
2024-02-19 13:14:01 +0800

    deploy setting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
-import GlobalStyles from './components/styles/GlobalStyles';
-import { theme } from './Global';
-import { ThemeProvider } from 'styled-components';
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import GlobalStyles from "./components/styles/GlobalStyles";
+import { theme } from "./Global";
+import { ThemeProvider } from "styled-components";
 function App() {
-	return (
-		<ThemeProvider theme={theme}>
-			<GlobalStyles />
-			<RouterProvider router={router}></RouterProvider>
-		</ThemeProvider>
-	);
+  return (
+    <RouterProvider router={router}>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}></ThemeProvider>
+    </RouterProvider>
+  );
 }
 
 export default App;
