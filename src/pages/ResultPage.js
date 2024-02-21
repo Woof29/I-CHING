ac8421e061e4f33e49f15fb5930e206d6895d7b0
Kevin Chen <kevin8003129@gmail.com>
2024-02-21 17:57:32 +0800

    firebase setting

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,29 +1,38 @@
-import { useEffect } from 'react';
-import Container from '../components/styles/Container.styled';
-import OpenFadeIn from '../components/styles/FadeIn';
-import firebase from '../utils/firebase';
-import 'firebase/compat/firestore';
+import { useEffect } from "react";
+import { useState } from "react";
+import Container from "../components/styles/Container.styled";
+import OpenFadeIn from "../components/styles/FadeIn";
+import { db } from "../utils/firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 const ResultPage = () => {
-	useEffect(() => {
-		firebase
-			.firestore()
-			.collection('hexagram')
-			.get()
-			.then((collectionSnapshot) => {
-				const data = collectionSnapshot.docs.map((docSnapshot) => {
-					return docSnapshot.data();
-				});
-				console.log(data);
-			});
-	});
-	return (
-		<OpenFadeIn>
-			<Container height="100dvh">
-				<h1>This is result</h1>
-			</Container>
-		</OpenFadeIn>
-	);
+  const [hexagram, setHexagram] = useState();
+
+  const getHexagram = async () => {
+    const querySnapshot = await getDocs(collection(db, "hexagram"));
+    const hexagramData = querySnapshot.docs.map((doc) => ({ ...doc.data() }));
+    setHexagram(hexagramData);
+    // console.log(hexagramData);
+  };
+
+  useEffect(() => {
+    getHexagram();
+  }, []);
+
+  return (
+    <OpenFadeIn>
+      <Container height="100dvh">
+        <h1>This is result</h1>
+        {/* {hexagram &&
+          hexagram.map((item, i) => (
+            <div key={i}>
+              <h1>{item.name}</h1>
+              <p>{item.judgment}</p>
+            </div>
+          ))} */}
+      </Container>
+    </OpenFadeIn>
+  );
 };
 
 export default ResultPage;
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,16 +1,24 @@
-import firebase from 'firebase/compat/app';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: 'AIzaSyBF-idro_9ZGkaFhBsLkK6fcm8d6rM7PIM',
-	authDomain: 'i-ching-1223e.firebaseapp.com',
-	projectId: 'i-ching-1223e',
-	storageBucket: 'i-ching-1223e.appspot.com',
-	messagingSenderId: '968613334298',
-	appId: '1:968613334298:web:aedbd4bad33d84159430a3',
-	measurementId: 'G-S6HE0DFM7Y',
+  apiKey: "AIzaSyBF-idro_9ZGkaFhBsLkK6fcm8d6rM7PIM",
+  authDomain: "i-ching-1223e.firebaseapp.com",
+  projectId: "i-ching-1223e",
+  storageBucket: "i-ching-1223e.appspot.com",
+  messagingSenderId: "968613334298",
+  appId: "1:968613334298:web:aedbd4bad33d84159430a3",
+  measurementId: "G-S6HE0DFM7Y",
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
-export default firebase;
+// name string
+// description string
+// id number
+// judgment string
+// lines array
+// linesMeaning array
+// 「乾=1、兌=2、離=3、震=4、巽=5、坎=6、艮=7、坤=8」
