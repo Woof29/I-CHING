ca0556806a4251de31dbf7b12677868d3bcb53be
Kevin <kevin8003129@gmail.com>
2024-02-27 01:27:15 +0800

    import i18n and setting

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,35 +1,36 @@
-import styled from "styled-components";
-import { theme } from "../Global";
-import Container from "../components/styles/Container.styled";
-import PrimaryButton from "../components/styles/PrimaryButton.styled";
+import styled from 'styled-components';
+import { theme } from '../Global';
+import Container from '../components/styles/Container.styled';
+import PrimaryButton from '../components/styles/PrimaryButton.styled';
+import { useTranslation } from 'react-i18next';
 
 const Title = styled.h1`
-  font-size: ${theme.font.mainTitle.size};
-  font-weight: ${theme.font.mainTitle.weight};
-  line-height: ${theme.font.mainTitle.lineHeight};
+	font-size: ${theme.font.mainTitle.size};
+	font-weight: ${theme.font.mainTitle.weight};
+	line-height: ${theme.font.mainTitle.lineHeight};
 `;
 
 const HomePage = () => {
-  return (
-    <Container>
-      <Title>
-        I CHING
-        <br />
-        Web of Changes
-      </Title>
-      <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-        <PrimaryButton
-          to="/notice"
-          flex="none"
-          width="255px"
-          bg={theme.color.primary}
-          hover={theme.color.primaryHover}
-        >
-          START
-        </PrimaryButton>
-      </div>
-    </Container>
-  );
+	const { t } = useTranslation();
+	return (
+		<Container>
+			<Title>
+				{t('homepage.title')}
+				<br />
+				{t('homepage.subtitle')}
+			</Title>
+			<div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+				<PrimaryButton
+					to="/notice"
+					flex="none"
+					width="255px"
+					bg={theme.color.primary}
+					hover={theme.color.primaryHover}>
+					START
+				</PrimaryButton>
+			</div>
+		</Container>
+	);
 };
 
 export default HomePage;
